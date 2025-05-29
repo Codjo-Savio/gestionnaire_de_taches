32d29403ac51d3087d69aaabca92dccb974d1f7a
Codjo-Savio <codjosavio@gmail.com>
2025-05-30 00:08:46 +0200

    modifs

--- galaxytasks-api/src/main/java/com/galaxytasks/model/Projet.java
+++ galaxytasks-api/src/main/java/com/galaxytasks/model/Projet.java
@@ -1,8 +1,9 @@
 package com.galaxytasks.model;
-
-import java.sql.Date;
-
+import java.time.LocalDate;
 import jakarta.persistence.*;
+
+@Entity
+@Table(name = "projet")
 public class Projet {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,10 +16,76 @@ public class Projet {
     private String descriptionProjet;
 
     @Column(nullable = false)
-    private Date date;
+    private LocalDate date_creation;
 
-    @Column(nullable = false)
-    private Integer idProprietaire;
+    @ManyToOne
+    @JoinColumn(name = "idProprietaire", nullable = false)
+    private Utilisateur idUtilisateur;
+
+    // constructeurs
+    private Projet(){
+        // constructeur vide pour JPA
+    }
+
+    private Projet(Integer idProjet, String intitule, String descriptionProjet, LocalDate date_creation, Utilisateur idUtilisateur){
+        this.idProjet = idProjet;
+        this.intitule = intitule;
+        this.descriptionProjet = descriptionProjet;
+        this.date_creation = date_creation;
+        this.idUtilisateur = idUtilisateur;
+    }
+
+    @Override
+    public String toString() {
+        return "Utilisateur{" +
+                "id=" + idProjet +
+                ", intitulé='" + intitule + '\'' +
+                ",Description='" + descriptionProjet + '\'' +
+                ",Date de création='" + date_creation + '\'' +
+                ",id Utilisateur='" + idUtilisateur + '\''+
+                '}';
+    }
+
+    // setters et getters
+    public void setIdProjet(Integer idProjet){
+        this.idProjet = idProjet;
+    }
+
+    public Integer getIdProjet(){
+        return idProjet;
+    }
+
+    public void setIntitule(String intitule){
+        this.intitule = intitule;
+    }
+
+    public String getIntitule(){
+        return intitule;
+    }
+
+    public void setDescription(String descriptionProjet){
+        this.descriptionProjet = descriptionProjet;
+    }
+
+    public String getDescription(){
+        return descriptionProjet;
+    }
+
+    public void setDateCreation(LocalDate date_creation){
+        this.date_creation = date_creation;
+    }
+
+    public LocalDate getDateCreation(){
+        return date_creation;
+    }
+
+    public void setProprietaire(Utilisateur idUtilisateur){
+        this.idUtilisateur = idUtilisateur;
+    }
+
+    public Utilisateur getProprietaire(){
+        return idUtilisateur;
+    }
 }
 /*
  * CREATE TABLE projet(
--- galaxytasks-api/src/main/java/com/galaxytasks/model/Tache.java
+++ galaxytasks-api/src/main/java/com/galaxytasks/model/Tache.java
@@ -1,7 +1,70 @@
 package com.galaxytasks.model;
-
+import java.time.LocalDate;
 import jakarta.persistence.*;
 
+@Entity
+@Table(name = "tache")
 public class Tache {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idTache;
+
+    @Column(nullable = false, length = 100)
+    private String titre;
+
+    @Column(nullable = true)
+    private String description_tache;
     
+    @Column(nullable = true)
+    private LocalDate date_echeance;
+
+    @Column(nullable = false)
+    private char[] priorite = {'1','2', '3', '4', '5'};
+
+    @Column (nullable = false)
+    private boolean est_termine = false;
+
+    @ManyToOne
+    @JoinColumn(name = "idProjet", nullable = false)
+    private Projet idProjet;
+
+    @ManyToOne
+    @JoinColumn(name = "idProprietaire", nullable = false)
+    private Utilisateur proprietaire;
+
+    // constructeurs
+    public Tache(){
+        // constructeur vide pour JPA
+    }
+
+    public Tache(Integer idTache, String titre, String description_tache, LocalDate date_echeance, char[] priorite, boolean est_termine, Projet idProjet, Utilisateur proprietaire){
+        this.idTache = idTache;
+        this.titre = titre;
+        this.description_tache = description_tache;
+        this.date_echeance = date_echeance;
+        this.priorite = priorite;
+        this.est_termine = est_termine;
+        this.idProjet = idProjet;
+        this.proprietaire = proprietaire;
+    }
 }
+
+
+/*
+ * CREATE TABLE tache(
+    idTache INT PRIMARY KEY AUTO_INCREMENT, -- AUTO_INCREMENT pour générer un identifiant unique à chaque ligne
+    titre VARCHAR(100) NOT NULL,
+    description_tache TEXT,
+    date_echeance DATE,
+    priorite ENUM('1','2','3','4','5') NOT NULL, -- Liste des valeurs autorisées
+    est_termine BOOLEAN DEFAULT FALSE,
+    idProjet INT NOT NULL,
+    idProprietaire INT,
+    FOREIGN KEY (idProjet) REFERENCES projet(idProjet)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (idProprietaire) REFERENCES utilisateur(idUtilisateur)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
+)ENGINE=InnoDB;
+ */
